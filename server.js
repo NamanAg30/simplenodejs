fd6b34772b222eb3eb3350276f545bfcd67f6e10
NamanAg30 <naman.agarwal75@gmail.com>
2025-02-12 12:46:36 +0530

    Initial commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello from the backend.Hi I am Naman Agarwal.Devops Enginner');
+    res.send('Hello from the backend.Hi I am Naman Agarwal.Devops Enginner at SensibleT');
 });
 
 app.listen(PORT, () => {
